da335039322ff1ed30c89e64df4e51ad7430547f
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2018-08-10 15:54:55 +0200

    Create a new user specific collection for each user.

--- aeolus/models.py
+++ aeolus/models.py
@@ -38,12 +38,15 @@ from django.db.models import (
 )
 from django.contrib.gis import geos
 from django.contrib.gis.db.models import (
-    GeoManager, MultiLineStringField,
+    GeoManager, MultiLineStringField, OneToOneField
 )
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_migrate
 
 from eoxserver.resources.coverages.models import (
-    collect_eo_metadata, Collection, Coverage, EO_OBJECT_TYPE_REGISTRY
+    collect_eo_metadata, Collection, Coverage, RangeType,
+    EO_OBJECT_TYPE_REGISTRY
 )
 
 
@@ -97,6 +100,8 @@ EO_OBJECT_TYPE_REGISTRY[301] = Product
 class ProductCollection(Product, Collection):
     objects = GeoManager()
 
+    user = OneToOneField(User, null=True, blank=True, default=None, related_name="user_collection")
+
     class Meta:
         verbose_name = "Product Collection"
         verbose_name_plural = "Product Collections"
@@ -126,3 +131,32 @@ class ProductCollection(Product, Collection):
         self.save()
 
 EO_OBJECT_TYPE_REGISTRY[310] = ProductCollection
+
+
+def get_or_create_user_product_collection(user):
+    range_type, _ = RangeType.objects.get_or_create(name="user_range_type")
+    collection, _ = ProductCollection.objects.get_or_create(
+        user=user,
+        identifier="user_collection_%s" % user.username,
+        min_x=-180,
+        min_y=-90,
+        max_x=180,
+        max_y=90,
+        srid=4326,
+        size_x=0,
+        size_y=1,
+        range_type=range_type,
+    )
+    return collection
+
+
+@receiver(post_migrate)
+def post_migrate_receiver(*args, **kwargs):
+    for user in User.objects.all():
+        get_or_create_user_product_collection(user)
+
+
+@receiver(post_save)
+def post_save_receiver(sender, instance, created, *args, **kwargs):
+    if issubclass(sender, User) and created:
+        get_or_create_user_product_collection(instance)
